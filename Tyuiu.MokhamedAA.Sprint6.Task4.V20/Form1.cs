f1fb76a4afeba3691ce53dbcec51c65bbce5a601
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-05 15:48:43 +0300

    Task4

---
+++ Tyuiu.MokhamedAA.Sprint6.Task4.V20/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.MokhamedAA.Sprint6.Task4.V20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
