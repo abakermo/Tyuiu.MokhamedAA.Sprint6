85d6bbaa5c7e3acf289cedabc88289bee90e63ca
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-05 14:44:23 +0300

    Task2

---
+++ Tyuiu.MokhamedAA.Sprint6.Task2.V3.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.MokhamedAA.Sprint6.Task2.V3.Lib
+{
+    public class DataService : ISprint6Task2V3
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue + 1);
+            valueArray = new double[len];
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x + 1.2 == 0)
+                {
+                    valueArray[count] = 0;
+                    continue;
+                }
+                double y = Math.Round(Math.Sin(x) / (x + 1.2) + Math.Cos(x) * 7 * x - 2, 2);
+                valueArray[count] = y;
+                count++;
+
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.MokhamedAA.Sprint6.Task2.V3/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿using Tyuiu.MokhamedAA.Sprint6.Task2.V3.Lib;
+namespace Tyuiu.MokhamedAA.Sprint6.Task2.V3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private void buttonDone_KDM_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = Convert.ToInt32(textBoxStart_KDM.Text);
+                int stopStep = Convert.ToInt32(textBoxStop_KDM.Text);
+
+                int len = ds.GetMassFunction(startStep, stopStep).Length;
+                double[] valueArray;
+                valueArray = new double[len];
+                valueArray = ds.GetMassFunction(startStep, stopStep);
+
+                this.chartFunction_KDM.Titles.Add("График функции");
+                this.chartFunction_KDM.ChartAreas[0].AxisX.Title = "Ось X";
+                this.chartFunction_KDM.ChartAreas[0].AxisY.Title = "Ось Y";
+
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    this.dataGridViewTable_KDM.Rows.Add(Convert.ToString(startStep), Convert.ToString(valueArray[i]));
+                    this.chartFunction_KDM.Series[0].Points.AddXY(startStep, valueArray[i]);
+                    startStep++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonInfo_KDM_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 2 выполнил студент группы ИСТНб-23-1 Коротченко Д.М.", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+}
